be73d9c9396f4fff800c8187398304a96db69dba
Rasheek <sallanrasheek@gmail.com>
2024-06-20 11:48:28 +0530

    demo

--- comfy-store/vite.config.js
+++ comfy-store/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Hello world
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
 });
